fb8aaae0fc3172a066050b394518280deeaeb8f7
EllynChan <eve9323@163.com>
2025-09-05 11:06:34 -0700

    adding colors

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -8,8 +8,9 @@ public class ColorChange : MonoBehaviour
 {
     public Light dirLight;
     private Color White = new Color(1f, 1f, 1f); // white
-    private Color Blue = new Color(0.4157f, 0.3765f, 0.7843f); // good for sea, possibly beach
-    private Color Yellow = new Color(0.8431f, 0.7529f, 0.2667f); // good for forest; natural light
+    private Color Purple = new Color(0.835f, 0.784f, 1.000f); // good for sea, possibly beach
+    private Color Yellow = new Color(0.996f, 0.996f, 0.741f); // good for forest; natural light
+    private Color Blue = new Color(0.486f, 0.522f, 0.871f);
     private List<Color> colors = new List<Color>();
     public int i = 1;
     private float updateInterval = .5f; // cap color change to max once per this interval, else it flickers on hold
@@ -21,8 +22,11 @@ public class ColorChange : MonoBehaviour
     {
         // this is the order of the colors, perhaps the ground scenes can match this order
         colors.Add(White);
-        colors.Add(Blue);
         colors.Add(Yellow);
+        colors.Add(Blue);
+        colors.Add(Purple);
+        colors.Add(White);
+
     }
 
     private void OnEnable()
